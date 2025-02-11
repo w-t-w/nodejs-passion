b0a5c844a9a4ca3a6f07e4d5f6ff26c6ccd3cdd5
w-t-w <15866369958@163.com>
2025-02-11 11:33:50 +0800

    NodeJS 《第一课: 基础语法-内置对象与函数》部分已热情的学习研究完毕

---
+++ basic/general/index.js
@@ -0,0 +1,40 @@
+// console.log('hello geekBang!');
+//
+// console.log(Date);
+// console.log(Math);
+//
+// console.log(setTimeout);
+// console.log(setInterval);
+// // console.log(requestAnimationFrame);
+// console.log(setImmediate);
+//
+// console.log(process);
+// // 内置对象
+// console.log(process.version);
+// console.log(process.versions);
+// console.log(process.title);
+// console.log(process.env);
+// console.log(process.arch);
+// console.log(process.platform);
+// console.log(process.pid);
+// console.log(process.ppid);
+// console.log(process.argv);
+// console.log(process.argv0);
+// // 内置函数
+// console.log(process.kill);
+// console.log(process.exit);
+// console.log(process.abort);
+// console.log(process.hrtime);
+// console.log(process.nextTick);
+// console.log(process.cwd);
+// console.log(process.chdir);
+// console.log(process.on);
+// console.log(process.once);
+// console.log(process.stdin);
+// console.log(process.stdout);
+// console.log(process.stderr);
+// console.log(process.cpuUsage);
+// console.log(process.memoryUsage);
+// console.log(process.resourceUsage);
+
+console.log('NodeJS 《第一课: 基础语法-内置对象与函数》部分已热情的学习研究完毕!');
