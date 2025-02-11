cd7d65c0db82931387dafbefb3034c62ac892ad4
w-t-w <15866369958@163.com>
2025-02-11 21:50:02 +0800

    NodeJS 《第十一课: HTTP 请求》部分已热情的学习研究完毕

---
+++ network/http/complete/index.js
@@ -0,0 +1,18 @@
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+//
+// const PORT = 3000;
+//
+// const TEMPLATE_DIR = path.resolve(__dirname, './source/index.html');
+//
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'text/html'});
+//     response.end(fs.readFileSync(TEMPLATE_DIR, 'utf-8'));
+// });
+//
+// server.listen(PORT, () => {
+//     console.log(`The server is running at http://localhost:${PORT}!`);
+// });
+
+console.log('NodeJS 《第十一课: 完整的 HTTP 请求》部分已热情的学习研究完毕!如需重试,将以上代码注释解开即可!');
--- network/http/index.js
+++ network/http/index.js
@@ -2,3 +2,5 @@
 require('./simple');
 // 普通的 HTTP 请求
 require('./normal');
+// 完整的 HTTP 请求
+require('./complete');
