153f012bf73543d864e62accd068214837d7f6a0
w-t-w <15866369958@163.com>
2025-02-14 23:32:48 +0800

    NodeJS 《第三十课: algorithm 算法抽奖或者集卡》部分已热情的学习研究完毕

---
+++ algorithm/index.js
@@ -0,0 +1,53 @@
+// const lessonIds = [
+//     '136797',
+//     '136798',
+//     '136799',
+//     '136800',
+//     '136801',
+//     '136803',
+//     '136804',
+//     '136806',
+//     '136807',
+//     '136808',
+//     '136809',
+//     '141994',
+//     '143517',
+//     '143557',
+//     '143564',
+//     '143644',
+//     '146470',
+//     '146569',
+//     '146582'
+// ];
+//
+// // for (let i = 0; i < 3; i++) {
+// //     const random = Math.floor((Math.random() * lessonIds.length));
+// //     const lessonId = lessonIds[random];
+// //     console.log('lesson\'s id: ', lessonId);
+// //     lessonIds.splice(random, 1);
+// // }
+// //
+// // for (let i = 0; i < 10; i++) {
+// //     const random_main = Math.floor((Math.random() * lessonIds.length));
+// //     const lessonId_main = lessonIds[random_main];
+// //     console.log('main lesson\'s id: ', lessonId_main);
+// //     lessonIds.splice(random_main, 1);
+// // }
+//
+// for (let i = 0; i < 3; i++) {
+//     const random = Math.floor((Math.random() * lessonIds.length));
+//     const lessonId = lessonIds[random];
+//     console.log('lesson\'s id: ', lessonId);
+//     lessonIds[random] = lessonIds[lessonIds.length - 1];
+//     lessonIds.length--;
+// }
+//
+// for (let i = 0; i < 10; i++) {
+//     const random_main = Math.floor((Math.random() * lessonIds.length));
+//     const lessonId_main = lessonIds[random_main];
+//     console.log('main lesson\'s id: ', lessonId_main);
+//     lessonIds[random_main] = lessonIds[lessonIds.length - 1];
+//     lessonIds.length--;
+// }
+
+console.log('NodeJS 《第三十课: algorithm 算法抽奖或者集卡》部分已热情的学习研究完毕!如需重试,将以上代码注释解开即可!');
